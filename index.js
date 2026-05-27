bf0b12fee6eb111bca66e8c65321cb1e4c40eee0
MirandaCintia92 <mirandacintia092@gmail.com>
2026-05-27 02:21:23 -0300

    actualizacion pre-entrega

--- index.js
+++ index.js
@@ -19,25 +19,61 @@ const main = async() =>{
     const [recurso, id] = partes;
     console.log(recurso, id);
 
-    //todos los productos
 
-    if (method === "GET" && recurso === "products" && !id) {
-        const reponse = await fetch(apiUrl);
-        const data = await reponse.json();
+
+switch (method){
+
+    case "GET":
+    if (recurso === "products" && !id) {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
 
         console.log("todos los productos");
         console.log(data);
     }
 
-    if (method === "GET" && recurso === "products" && id) {
-        const reponse = await fetch(`${apiUrl}/${id}`)
-        const data = await reponse.json();
+    if (recurso === "products" && id) {
+        const response = await fetch(`${apiUrl}/${id}`)
+        const data = await response.json();
 
         console.log("Producto:");
         console.log(data);
     }
+    break;
 
+    case "POST":
+        if(recurso === "products"){
+        const title = process.argv[4];
+        const price = process.argv[5];
+        const category = process.argv[6];
+        
+        const product = {title, price: Number(price), category};
+
+        const response = await fetch(apiUrl,{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(product)
+        });
+
+        const data = await response.json();
+        console.log(data);
+        }
+        break; 
+        
+        case "DELETE":
+        if(recurso === "products" && id){
+        
+
+        const response = await fetch(apiUrl,{
+            method: "DELETE"
+        });
+
+        const data = await response.json();
+        console.log("producto eliminado");
+        console.log(data);
+        }
+        break;
+
+    }
 };
 main();
-
-
