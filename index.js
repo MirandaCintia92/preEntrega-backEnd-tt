0fec4ca3987d2760ffa7220d354e8dab4580527a
MirandaCintia92 <mirandacintia092@gmail.com>
2026-05-27 01:53:44 -0300

    actualizacion pre-entrega

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log(process.argv);
+//console.log(process.argv); //con esto obtengo los argumentos
+//const method = process.argv[2]; //metodo
+//const endpoint = process.argv[3]; //endpoint
+
+const apiUrl = "https://fakestoreapi.com/products";
+
+const main = async() =>{
+    const [, , method,endpoint] = process.argv;
+
+    console.log(`Method: ${method}`);  //GET
+    console.log(`EndPoint: ${endpoint}`); //products/7
+
+        if (!endpoint){
+            console.log("Falta Endpoint");
+            process.exit();
+        }
+
+    const partes = endpoint.split('/');
+    const [recurso, id] = partes;
+    console.log(recurso, id);
+
+    //todos los productos
+
+    if (method === "GET" && recurso === "products" && !id) {
+        const reponse = await fetch(apiUrl);
+        const data = await reponse.json();
+
+        console.log("todos los productos");
+        console.log(data);
+    }
+
+    if (method === "GET" && recurso === "products" && id) {
+        const reponse = await fetch(`${apiUrl}/${id}`)
+        const data = await reponse.json();
+
+        console.log("Producto:");
+        console.log(data);
+    }
+
+};
+main();
+
+
